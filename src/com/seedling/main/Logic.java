34dc70ccc418ae5a866bc10e2788e8672cf7d6d2
Christoffer Hansen <chris.hansen.ch@outlook.com>
2019-12-08 00:48:01 +0100

    Continued to develop the various menus.

--- src/com/seedling/main/Logic.java
+++ src/com/seedling/main/Logic.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class Logic {
 
     private final Scanner in = new Scanner(System.in);
-    private final CoffeeMachine coffeeMachine = new CoffeeMachine();
+    private final CoffeeMachine coffeeMachine = new CoffeeMachine(1000, 500, 200);
+
+    private final int[] espressoRecipe = {30, 0, 8};
+    private final int[] blackCoffeeRecipe = {300, 0, 20};
+    private final int[] latteRecipe = {60, 180, 16};
 
     public void startMenu() {
 
+        System.out.println("[Start menu]\n");
+
         OUTER:
         while (true) {
-            System.out.println("[1 = Buy | 2 = Ingredients | 3 = Fill | 0 = Exit]\n> ");
+            System.out.print("[1 = Buy | 2 = Ingredients | 3 = Fill | 0 = Exit]\n> ");
 
             switch (in.nextInt()) {
                 case 0: {
@@ -39,6 +45,7 @@ public class Logic {
     }
 
     private void buyMenu() {
+        System.out.println("[Buy menu]\n");
 
         OUTER:
         while (true) {
@@ -54,12 +61,15 @@ public class Logic {
                     break OUTER;
                 }
                 case 1: {
+                    buyBlackCoffee();
                     break;
                 }
                 case 2: {
+                    buyEspresso();
                     break;
                 }
                 case 3: {
+                    buyLatte();
                     break;
                 }
                 default: {
@@ -71,8 +81,40 @@ public class Logic {
 
     }
 
-    private void fillMenu() {
+    private void buyBlackCoffee() {
+        if (blackCoffeeRecipe[0] > coffeeMachine.getWater()) {
+            System.out.println("$> Not enough water in the machine. Please fill it up!");
+        } else if (blackCoffeeRecipe[2] > coffeeMachine.getBeans()) {
+            System.out.println("$> Not enough beans in the machine. Please fill it up!");
+        } else {
+            System.out.println("$> One Black coffee made.\nYou're welcome!");
 
+            coffeeMachine.setWater(-blackCoffeeRecipe[0]);
+            coffeeMachine.setBeans(-blackCoffeeRecipe[2]);
+
+        }
+    }
+
+    private void buyEspresso() {
+
+    }
+
+    private void buyLatte() {
+
+    }
+
+
+    private void fillMenu() {
+        System.out.println("[Fill menu]\n");
+
+        System.out.print("How much water (ml):\n> ");
+        coffeeMachine.setWater(in.nextInt());
+
+        System.out.print("How much milk (ml):\n> ");
+        coffeeMachine.setMilk(in.nextInt());
+
+        System.out.print("How much grams (g):\n> ");
+        coffeeMachine.setBeans(in.nextInt());
     }
 
 }
