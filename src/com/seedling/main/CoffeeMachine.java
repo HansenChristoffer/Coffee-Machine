0feea6fab51796e85288b8092faf3a3f8aaae6c1
Christoffer Hansen <chris.hansen.ch@outlook.com>
2019-12-08 00:04:19 +0100

    Added the Coffee Machine class that will handle any info/data about the machine.

---
+++ src/com/seedling/main/CoffeeMachine.java
@@ -0,0 +1,36 @@
+package com.seedling.main;
+
+public class CoffeeMachine {
+
+    int water;
+    int milk;
+    int beans;
+
+    public CoffeeMachine() {
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+
+    public int getBeans() {
+        return beans;
+    }
+
+    public void setBeans(int beans) {
+        this.beans = beans;
+    }
+
+}
