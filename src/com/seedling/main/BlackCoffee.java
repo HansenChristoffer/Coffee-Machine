b035988bfd88ced49ae8902010cf57a1416e7a1c
Christoffer Hansen <chris.hansen.ch@outlook.com>
2019-12-08 01:01:58 +0100

    Removed unnecessary classes such as the different coffee types.

--- src/com/seedling/main/BlackCoffee.java
+++
@@ -1,9 +0,0 @@
-package com.seedling.main;
-
-public class BlackCoffee extends Coffee {
-
-    public BlackCoffee() {
-        super(250, 0, 20);
-    }
-
-}
--- src/com/seedling/main/Espresso.java
+++
@@ -1,9 +0,0 @@
-package com.seedling.main;
-
-public class Espresso extends Coffee {
-
-    public Espresso() {
-        super(200, 50, 15);
-    }
-
-}
